03eef1469dcc44aea05853f635e2e23b31871343
soumya123ganguly <soumya123ganguly@gmail.com>
2023-02-28 03:46:19 +0000

    resnet50 implemented with linear layer

---
+++ resnet50.py
@@ -0,0 +1,30 @@
+import torch.nn as nn
+from torchvision import models 
+from torchsummary import summary
+
+#ToDO Fill in the __ values
+class Resnet50FCN(nn.Module):
+
+    def __init__(self):
+        super().__init__()
+        self.model = models.resnet50(pretrained=True)
+        print(self.model)
+        for child in self.model.children():
+          for param in child.parameters():
+            param.requires_grad = False
+        self.fc1 = nn.Linear(2048*8*8, 300)
+
+#TODO Complete the forward pass
+    def forward(self, x):
+        x = self.model.conv1(x)
+        x = self.model.bn1(x)
+        x = self.model.maxpool(x)
+        x = self.model.layer1(x)
+        x = self.model.layer2(x)
+        x = self.model.layer3(x)
+        x = self.model.layer4(x)
+        x = self.fc1(x.reshape(-1, 2048*8*8))
+        return x
+
+net=Resnet50FCN().cuda()
+summary(net, (3, 256, 256))
